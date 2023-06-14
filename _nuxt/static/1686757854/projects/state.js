566d5b33263ff0a8181e9e2c90cef9d3441c3372
Jabari Crenshaw <36088613+jabaricrenshaw@users.noreply.github.com>
2023-06-14 08:52:21 -0700

    Images Fix Final

---
+++ _nuxt/static/1686757854/projects/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FPersonal_Website\u002F_nuxt\u002Fstatic\u002F1686757854",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprojects",config:{_app:{basePath:"\u002FPersonal_Website\u002F",assetsPath:"\u002FPersonal_Website\u002F_nuxt\u002F",cdnURL:a}}}}(null));
